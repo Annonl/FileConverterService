a2af20a3774309d57240ddf0f1b0f06335d91366
Alexey <alesha.kolpashikov@gmail.com>
2021-11-05 11:03:38 +0300

    main console application written

--- FileConvertService/Program.cs
+++ FileConvertService/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Linq;
+using FileConverterServiceLibrary;
 
 namespace FileConvertService
 {
@@ -6,7 +10,41 @@ namespace FileConvertService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstFile, secondFile;
+            switch (args.Length)
+            {
+                case 0:
+                    Console.WriteLine("Enter the name of the file where you want to take data from:");
+                    firstFile = Console.ReadLine();
+                    Console.WriteLine("Enter the name of the file where you want to write the data:");
+                    secondFile = Console.ReadLine();
+                    break;
+                case 1:
+                    firstFile = args[0];
+                    Console.WriteLine("Enter the name of the file where you want to write the data:");
+                    secondFile = Console.ReadLine();
+                    break;
+                default:
+                    firstFile = args[0];
+                    secondFile = args[1];
+                    break;
+            }
+
+            if (File.Exists(firstFile))
+            {
+                if (firstFile.Split('.').Last() == "xml")
+                {
+                    DeserializeXml sDeserializeXml = new DeserializeXml();
+                    var shop = sDeserializeXml.DeserializeObject(firstFile);
+                    shop = ChangeStructureShop.ChangeForJson(shop);
+                    SerializationJSON serializationJson = new SerializationJSON();
+                    serializationJson.SerializeObject(secondFile,shop);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The entered files were not found. Try to enter absolute file paths.");
+            }
         }
     }
 }
